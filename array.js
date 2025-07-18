a45919ceac53d5f89c6abb3560391452da34c8c0
Oznur <oznur@Oznurs-MacBook-Pro.local>
2025-07-18 14:58:23 -0500

    Add array.js with grocery list and prices arrays

---
+++ array.js
@@ -0,0 +1,9 @@
+let groceryList = ["Milk", "Butter", "Pasta", "Water", "Bread"];
+let prices = [5.00, 10.00, 7.00];
+
+console.log("Grocery items:", groceryList);
+console.log("Prices:", prices);
+console.log("Milk:", groceryList[0]);
+console.log("Butter:", groceryList[1]);
+console.log("Pasta:", groceryList[2]);
+
